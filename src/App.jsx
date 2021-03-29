22bd5f210455ed8b1331e9a58e297cec345403d5
Márcio José Gurka Júnior <marciogurkajr@gmail.com>
2021-03-28 21:49:33 -0300

    WIP: Finishing section 'Conceitos Importantes'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import './styles/global.scss';
+import { Counter } from "./components/Counter";
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
 
 export function App() {
-  return <h1>Teste</h1>;
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
